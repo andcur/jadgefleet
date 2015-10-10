fba8aa217be2c845bf8588b104b09caf2ce0a894
andcur <andcur@hotmail.com>
2015-10-10 22:55:36 +0200

    Timing tweaks

--- kickstarter.sh
+++ kickstarter.sh
@@ -9,39 +9,32 @@ echo "Starting MongoDB nodes for replicaset 1 and 2..."
 fleetctl start $DIR/rs1@{1..5}.service
 fleetctl start $DIR/rs2@{1..5}.service
 
-echo "Sleep 30: Waiting for nodes to finish startup..."
-sleep 30
-
-echo "Configuring replicaset 1 and 2..."
+echo "Sleeping 120s before configuring replicaset 1 and 2..."
+sleep 120
 fleetctl start $DIR/mongoreplicacfg@rs1.service
 fleetctl start $DIR/mongoreplicacfg@rs2.service
 
 echo "Starting configservers 1, 2 and 3..."
 fleetctl start $DIR/mongocfgsvr@{1..3}.service
 
-echo "Sleep 30: Waiting for configservers to finish startup..."
-sleep 30
-
-echo "Starting mongos 1, 2 and 3..."
+echo "Sleeping 120s before starting mongos 1, 2 and 3..."
+sleep 120
 fleetctl start $DIR/mongos@{1..3}.service
 
-echo "Sleep 15: Waiting for mongos to finish startup..."
-sleep 15
-
-echo "Registering shard 1 and 2 with DB cluster..."
+echo "Sleeping 60s before registering shard 1 and 2 with DB cluster..."
+sleep 60
 fleetctl start $DIR/mongoaddshard@rs1.service
 fleetctl start $DIR/mongoaddshard@rs2.service
 
-echo "Sleep 10: Waiting for shardregistering to finish ..."
-sleep 10
-
-echo "Insert basic JADGe data and setting shardkey for balancing..."
+echo "Sleeping 30s before inserting basic JADGe data and setting shardkey for balancing..."
+sleep 30
 fleetctl start $DIR/initJadgeDB.service
 
-echo "Starting littlehelper backends 1, 2 and 3..."
-fleetctl start $DIR/littlehelper@6666.service
-fleetctl start $DIR/littlehelper@7777.service
-fleetctl start $DIR/littlehelper@8888.service
+echo "Sleeping 60s before starting littlehelper backends 1, 2 and 3..."
+sleep 60
+fleetctl start $DIR/littlehelper@1.service
+fleetctl start $DIR/littlehelper@2.service
+fleetctl start $DIR/littlehelper@3.service
 fleetctl start $DIR/littlehelper-discovery@6666.service
 fleetctl start $DIR/littlehelper-discovery@7777.service
 fleetctl start $DIR/littlehelper-discovery@8888.service
